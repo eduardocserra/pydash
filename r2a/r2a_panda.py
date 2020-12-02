bc5098b53ad4d7b8e5cbe72f82552e4622c6b840
Eduardo Castro Serra <eduardocserra@outlook.com>
2020-12-02 18:39:20 -0300

    Adicionado amortecimento no throughput

--- r2a/r2a_panda.py
+++ r2a/r2a_panda.py
@@ -10,6 +10,7 @@ class R2A_Panda(IR2A):
         IR2A.__init__(self, id)
         self.throughputs = []
         self.calc_throughputs = []
+        self.smooth_throughputs = []
         self.time_download = []
         self.inter_request_time = []
         self.qi = []
@@ -28,34 +29,42 @@ class R2A_Panda(IR2A):
         if len(self.throughputs) == 0:
             self.throughputs.append(msg.get_bit_length() / t)
             self.calc_throughputs.append(msg.get_bit_length() / t)
+            self.smooth_throughputs.append(msg.get_bit_length() / t)
 
         self.send_up(msg)
 
     def handle_segment_size_request(self, msg):
         self.request_time = time.perf_counter()
         x = 0
-        w = 0.3 * 1000000
+        w = 0.35 * 1000000
         k = 0.14  #0.04, 0.07, 0.14 até 0.56, aumentando 0.14
         E = 0.15
+        alfa = 0.2
+        y = self.throughputs[0]
         if len(self.throughputs) == 1:
             x = self.throughputs[0]
         else:
-            x = (w - max((0, self.calc_throughputs[-1] - self.throughputs[-1] + w))
-                 ) * k * self.inter_request_time[-1] + self.calc_throughputs[-1]
+            x = abs((w - max((0, self.calc_throughputs[-1] - self.throughputs[-1] + w))
+                 ) * k * self.inter_request_time[-1] + self.calc_throughputs[-1])
+            y = -alfa * \
+                (self.smooth_throughputs[-1] - x) * \
+                self.inter_request_time[-1] + self.smooth_throughputs[-1]
             self.calc_throughputs.append(x)
-        
+            self.smooth_throughputs.append(y)
+            
         print(f'real_throughput={self.throughputs}')
         print(f'calc_throughput={self.calc_throughputs}')
+        print(f'smooth_throughput={self.smooth_throughputs}')
         selected_qi = self.qi[0]
         for i in self.qi:
-            if x > i:
+            if y > i:
                 selected_qi = i
         print(f'qi={selected_qi}')
         msg.add_quality_id(selected_qi)
         self.send_down(msg)
 
     def handle_segment_size_response(self, msg):
-        buffer_min = 26
+        buffer_min = 55
         beta = 0.2
         B = self.whiteboard.get_amount_video_to_play()
         if len(self.throughputs) == 1:
